9495b5adb2b1f694fd6e300d16846f23bb7ab533
CLHEX <clhexftw@protonmail.com>
2022-03-21 08:45:47 +0530

    vayu: CipherOS Bringup
    Signed-off-by: CLHEX <clhexftw@protonmail.com>

--- cipher_vayu.mk
+++ cipher_vayu.mk
@@ -11,10 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common CipherOS configurations
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_vayu
+
+PRODUCT_NAME := cipher_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -29,7 +30,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:11/RKQ1.200826.002/V12.5.7.0.RJUMIXM:user/release-keys
 
-# ArrowOS additions
-DEVICE_MAINTAINER := kubersharma001
-$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+# CipherOS additions
+
+# Maintainer
+CIPHER_MAINTAINER := CLHEX
+
+# Boot Res
+TARGET_BOOT_ANIMATION_RES := 1080
 
